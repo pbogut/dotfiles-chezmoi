a338e8d8d41f1127aa3dc40f436ca1d1e011f584
Pawel Bogut <pbogut@pbogut.me>
2024-03-06 09:34:47 +0100

    feat: fix systemctl

--- run_onchange_after_systemctl.sh
+++ run_onchange_after_systemctl.sh
@@ -4,7 +4,5 @@
 # author: pbogut <pbogut@pbogut.me>
 # date:   06/03/2024
 #=================================================
-sudo systemctl enable --now dbus-broker.service
 sudo systemctl --global disable gnome-keyring-daemon.socket
-
 systemctl --user enable gnome-keyring-daemon.service
